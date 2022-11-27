a155c266a8fc55161dd8bc8bc9d3a801b90c5def
malihaafs <mafsa@my.centennialcollege.ca>
2022-11-28 02:10:40 +0600

    Authentication

---
+++ backend/controllers/authController.js
@@ -0,0 +1,35 @@
+const User = require('../models/user');
+
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+
+// Register a user  =? /api/v1/register
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+    const { name, email, password } = req.body;
+
+    const user = await User.create({
+        name, email, password, 
+        avatar: {
+            public_id: 'samples/ecommerce/leather-bag-gray.jpg',
+            url: 'https://res.cloudinary.com/dhxbzj2st/image/upload/v1665850995/samples/ecommerce/leather-bag-gray.jpg'
+        }
+    });
+    
+    const token = user.getJwtToken();
+
+    res.status(201).json({
+        success: true,
+        token
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
