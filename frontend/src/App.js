aa0f554681e75e15b54adaa84213fb1f5ed05e0e
SAhmedNaim <naim.ahmed035@gmail.com>
2022-12-25 04:19:11 +0600

    Product details component, action & reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
+import ProductDetails from "./components/product/ProductDetails";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <div className="container container-fluid">
                 <Header />
                 <Route path="/" component={Home} exact />
+                <Route path="/product/:id" component={ProductDetails} exact />
             </div>
             <Footer />
         </Router>
