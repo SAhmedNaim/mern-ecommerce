78ae73c9a761d061b8c3fa1d172f1086834df028
SAhmedNaim <naim.ahmed035@gmail.com>
2022-12-25 17:41:41 +0600

    Price Filter by Range

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Pagination from 'react-js-pagination';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import MetaData from './layout/MetaData';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../actions/productActions';
@@ -7,9 +9,14 @@ import Product from './product/Product';
 import Loader from './layout/Loader';
 import { useAlert } from 'react-alert';
 
+const { createSliderWithTooltip } = Slider;
+// const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
+
 const Home = ({ match }) => {
 
     const [currentPage, setCurrentPage] = useState(1);
+    const [price, setPrice] = useState([1, 1000]);
 
     const alert = useAlert();
     const dispatch = useDispatch();
@@ -24,9 +31,9 @@ const Home = ({ match }) => {
             return alert.error(error);
         }
 
-        dispatch(getProducts(keyword, currentPage));
+        dispatch(getProducts(keyword, currentPage, price));
 
-    }, [dispatch, alert, error, keyword, currentPage]);
+    }, [dispatch, alert, error, keyword, currentPage, price]);
 
     function setCurrentPageNo(pageNumber) {
         setCurrentPage(pageNumber);
@@ -41,9 +48,44 @@ const Home = ({ match }) => {
 
                     <section id="products" className="container mt-5">
                         <div className="row">
-                            {products && products.map(product => (
-                                <Product key={product._id} product={product} />
-                            ))}
+
+                            {keyword ? (
+                                <>
+                                    <div className='col-6 col-md-3 mt-5 mb-5'>
+                                        <div className='px-5'>
+                                            <Range
+                                                marks={{
+                                                    1: `$1`,
+                                                    1000: `$1000`
+                                                }}
+                                                min={1}
+                                                max={1000}
+                                                defaultValue={[1, 1000]}
+                                                tipFormatter={value => `$${value}`}
+                                                tipProps={{
+                                                    placement: "top",
+                                                    visible: true
+                                                }}
+                                                value={price}
+                                                onChange={price => setPrice(price)}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className='col-6 col-md-9'>
+                                        <div className='row'>
+                                            {products && products.map(product => (
+                                                <Product key={product._id} product={product} col={4} />
+                                            ))}
+                                        </div>
+                                    </div>
+                                </>
+                            ) : (
+                                products && products.map(product => (
+                                    <Product key={product._id} product={product} col={3} />
+                                ))
+                            )}
+
                         </div>
                     </section>
 
--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Search from './Search';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import '../../App.css';
 
 const Header = () => {
@@ -9,7 +9,9 @@ const Header = () => {
             <nav className="navbar row">
                 <div className="col-12 col-md-3">
                     <div className="navbar-brand">
-                    <img src="/images/logo.png" />
+                        <Link to="/">
+                            <img src="/images/logo.png" />
+                        </Link>
                     </div>
                 </div>
 
