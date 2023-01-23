b02411773cc3b250a597c91b2190b0dcf4376442
SAhmedNaim <naim.ahmed035@gmail.com>
2023-01-24 02:35:21 +0600

    Delete Orders

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -16,6 +16,11 @@ import {
     UPDATE_ORDER_RESET,
     UPDATE_ORDER_FAIL,
 
+    DELETE_ORDER_REQUEST,
+    DELETE_ORDER_SUCCESS,
+    DELETE_ORDER_RESET,
+    DELETE_ORDER_FAIL,
+
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
     ORDER_DETAILS_FAIL,
@@ -156,6 +161,7 @@ export const orderReducer = (state = {}, action) => {
     switch (action.type) {
 
         case UPDATE_ORDER_REQUEST:
+        case DELETE_ORDER_REQUEST:
             return {
                 ...state,
                 loading: true
@@ -168,7 +174,15 @@ export const orderReducer = (state = {}, action) => {
                 isUpdated: action.payload
             };
 
+        case DELETE_ORDER_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                isDeleted: action.payload
+            };
+
         case UPDATE_ORDER_FAIL:
+        case DELETE_ORDER_FAIL:
             return {
                 ...state,
                 error: action.payload
@@ -178,6 +192,12 @@ export const orderReducer = (state = {}, action) => {
             return {
                 ...state,
                 isUpdated: false
+            };
+
+        case DELETE_ORDER_RESET:
+            return {
+                ...state,
+                isDeleted: false
             };
 
         case CLEAR_ERRORS:
